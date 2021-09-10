cee52d6a1348cf16112916874a1c3f0b4002f0e7
dcrhodes <sanooke@me.com>
2021-09-09 21:45:35 -0400

    adds js dom manipulation on the sport detakils page

--- main_app/static/js/games_detail.js
+++ main_app/static/js/games_detail.js
@@ -10,15 +10,3 @@ const picker = MCDatepicker.create({
 dateInput.addEventListener("click", (evt) => {
   picker.open()
 })
-
-// const fileInput = document.getElementById('file-input')
-// const fileName = document.getElementById('file-name')
-
-// fileInput.addEventListener('change', evt => {
-//   const fileToUpload = evt.target.files[0].name
-//   if(fileToUpload) {
-//     fileName.innerText = fileToUpload
-//   } else {
-//     fileName.innerText = ""
-//   }
-// })
---
+++ main_app/static/js/sport.js
@@ -0,0 +1,29 @@
+function caber(){
+  document.getElementById('sportimage')
+  .src="/static/images/sport_caber.jpeg"
+}
+
+function stoneput(){
+  document.getElementById('sportimage')
+  .src="/static/images/sport_stoneput.jpg"
+}
+
+function wfd(){
+  document.getElementById('sportimage')
+  .src="/static/images/sport_wfd.jpg"
+}
+
+function hammers(){
+  document.getElementById('sportimage')
+  .src="/static/images/sport_hammer.jpeg"
+}
+
+function wob(){
+  document.getElementById('sportimage')
+  .src="/static/images/sport_wob.jpeg"
+}
+
+function sheaf(){
+  document.getElementById('sportimage')
+  .src="/static/images/sport_sheaf.png"
+}
